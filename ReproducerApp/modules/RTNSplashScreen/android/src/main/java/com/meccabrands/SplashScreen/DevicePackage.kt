0362ae8938e578abf11b382f3bb3a6aef0458196
Aldrin Tingson <aldrin.tingson@mecca.com.au>
2024-08-05 18:25:34 +0800

    fixed typo

--- ReproducerApp/modules/RTNSplashScreen/android/src/main/java/com/meccabrands/SplashScreen/DevicePackage.kt
+++ ReproducerApp/modules/RTNSplashScreen/android/src/main/java/com/meccabrands/SplashScreen/DevicePackage.kt
@@ -3,7 +3,7 @@ package com.meccabrands.SplashScreen;
 import com.facebook.react.TurboReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.module.model.ReacTurboModuleoduleInfoProvider
+import com.facebook.react.module.model.ReactTurboModuleInfoProvider
 
 class DevicePackage : TurboReactPackage() {
   override fun getModule(name: String?, reactContext: ReactApplicationContext): NativeModule? = null
